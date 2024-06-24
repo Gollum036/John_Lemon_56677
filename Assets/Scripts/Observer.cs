e084d65f2f68eeffad231e4f4ef73d93a7934f24
Gollum036 <pedromiguel.dfonseca@gmail.com>
2024-06-24 02:14:41 +0100

    7.Check the Enemy's Line of Sight is Clear

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public GameEnding gameEnding;
     bool m_IsPlayerInRange;
 
     void OnTriggerEnter(Collider other)
@@ -20,5 +21,22 @@ public class Observer : MonoBehaviour
         {
             m_IsPlayerInRange = false;
         }
+    }
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+
+                }
+            }
+        }
+
     }
 }
